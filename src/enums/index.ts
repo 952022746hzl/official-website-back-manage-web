aa9a08a2cf5d4cb8be1284af940ffac975151c83
hzl <952022746@qq.com>
2025-12-08 15:49:10 +0800

    init

---
+++ src/enums/index.ts
@@ -0,0 +1,9 @@
+export * from "./codegen/form-enum";
+export * from "./codegen/query-enum";
+
+export * from "./settings/layout-enum";
+export * from "./settings/theme-enum";
+export * from "./settings/locale-enum";
+export * from "./settings/device-enum";
+
+export * from "./system/menu-enum";
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard-container">
+    This is dashboard
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "Dashboard",
+  inheritAttrs: false,
+});
+</script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  position: relative;
+  padding: 24px;
+}
+</style>
